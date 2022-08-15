27e5247818407e60344ea6992415ac7b9ee66bfa
Jonathan Chow <jonathan@cychow.co.uk>
2022-08-15 22:24:05 +0100

    Simplified config

--- app/api/config.py
+++ app/api/config.py
@@ -1,20 +1,10 @@
-import os
-import secrets
 from dotenv import load_dotenv
-
-from typing import Any, Dict, List, Optional, Union
-from pydantic import AnyHttpUrl, BaseSettings, EmailStr, HttpUrl, PostgresDsn, validator
+from pydantic import BaseSettings
 
 load_dotenv() 
 
 class Config(BaseSettings):
   
-
     DATETIME_FORMAT:str = "%Y-%m-%d"
-
-
-
-   
-
 
 config = Config()
